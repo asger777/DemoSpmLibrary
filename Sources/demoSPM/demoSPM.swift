50d4d659923315aa1d9582cc8c17c0b0080896c0
Asgar F. Amrahov <asgar.amrahov@kapitalbank.az>
2024-11-23 15:29:22 +0400

    first commit

---
+++ Sources/demoSPM/demoSPM.swift
@@ -0,0 +1,33 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import UIKit
+
+final public class DemoSpmViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set up the view's background color
+        view.backgroundColor = .white
+        
+        // Create and configure the UILabel
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        
+        // Enable Auto Layout for the label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the label to the view
+        view.addSubview(label)
+        
+        // Center the label in the view
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
